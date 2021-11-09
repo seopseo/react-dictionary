cf040c3dde58b0480dde3aaab7250425b4b5f3d7
seopseo <jk97104@naver.com>
2021-11-09 15:39:03 +0900

    new React

--- src/App.js
+++ src/App.js
@@ -1,73 +1,32 @@
+// import react from 'react'
 import './App.css';
-import React, { Component } from 'react';
-// import Movie from './Movie';
-import Word from './Word';
-
+import { Component } from 'react'
+import Nav from './Nav'
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            loading: true, 
-            words: [] 
-        } 
-    } 
-    
-    componentDidMount(){ 
-        fetch('https://dictionary-search-app.herokuapp.com/api/words') 
-        .then( res => res.json()) 
-        .then( result => { 
-            const {words} = result 
-            console.log(words) 
-            this.setState({loading: false, words}) 
-        }) 
-    } 
-    
-    render(){ 
-        const {loading, words} = this.state 
-        const style = { 
-            display: 'flex', 
-            flexWrap: 'wrap', 
-            justifyContent: 'center', 
-            alignItems: 'center', 
-            width: '60%',
-            margin: '100px auto', 
-            textAlign: 'center' 
-        } 
-        const loadingStyle = { 
-            position: 'absolute', 
-            left: '50%', 
-            top:'50%', 
-            transform: 'translate(-50%, -50%)', 
-            fontSize: '2rem' 
-        } 
-        if(loading){ 
-            return ( 
-            <div style={loadingStyle}> 
-            <h1>Loading ...</h1> 
-            </div> 
-            ) 
-        }else{ 
-          return ( 
-            <div style={style}> 
-              {words.map(word => { 
-                return ( 
-                    <Word 
-                    key={word._id}
-                    r_seq={word.r_seq} 
-                    r-word={word.r_word} 
-                    r_link={word.r_link} 
-                    r_chi={word.r_chi} 
-                    r_des={word.r_des} 
-                    r_pos={word.r_pos}
-                    ></Word> 
-                
-                 ) 
-              })} 
-           </div> 
-         ) 
-      } 
-    } 
-} 
-export default App;
+    state ={
+        menus: [
+            {
+                siteTitle:'구글',
+                siteUrl: 'https://google.com'
+            },
+            {
+                siteTitle:'네이버',
+                siteUrl: 'https://naver.com'
+            },
+            {
+                siteTitle:'사전검색',
+                siteUrl: 'https://seopseo.github.io/dictionary-front/'
+            }
+        ]
+    }
+    render(){
+        return (
+            <div className="App">
+                <Nav menus={this.state.menus}></Nav>
+            </div>
+        )
+    }
+}
 
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Button.css'
+
+function Button({ children, size, color, width, handleClick, disabled }){
+    return <button className={`Button ${size} ${color} ${width}`}
+                    onClick={handleClick} disabled={disabled}
+                >{children}</button>
+}
+
+export default Button;
+
+Button.defaultProps = {
+    size: 'medium',
+    color: 'tomato',
+    disabled: false
+}
